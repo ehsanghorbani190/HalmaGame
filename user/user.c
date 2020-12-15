7b880a802932c9dca06c76e2dad13eec5a23dd8c
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2020-12-15 20:08:31 +0330

    Added ShowLogin func

--- user/user.c
+++ user/user.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+
+
 struct User
 {
     char userName[50];
@@ -89,4 +95,54 @@ unsigned long GetPassword()
     };
 
     return hash(temp);
+}
+void ShowLogin(){
+    
+    char input;
+    unsigned int pointer=0,isLogin=0;
+    while(input!=13)
+    {
+
+        if(input==KEY_UP)
+            pointer++;
+        if(input==KEY_DOWN)
+            pointer--;
+        SetColor(4);
+        pointer%2 ? printf("  "):printf("> ");
+        printf("login\n");
+        SetColor(6);
+        pointer%2 ? printf("> "):printf("  ");
+        printf("register\n");
+        input = getch();
+        system("cls");
+
+    }
+
+    if(pointer%2)
+    {
+        struct User user;
+
+        printf("Plz enter your user name:");
+        scanf("%s",&user.userName);
+        printf("Plz enter your password:");
+
+        user.password=GetPassword();
+
+        save(user);
+
+
+    }
+    else
+    {
+        char username[50];
+        unsigned long password;
+        printf("Plz enter your user name:");
+        scanf("%s",&username);
+        printf("Plz enter your password:");
+        password=GetPassword();
+        isLogin=login(username,password);
+        printf("\n%d",isLogin);
+
+
+    }
 }
