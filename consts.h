d2ba161ff5c7e6106c1c59a45c0ff77246fcd010
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2020-12-16 10:51:00 +0330

    Created consts.h for constants and included it in main.c

---
+++ consts.h
@@ -0,0 +1,6 @@
+#define boardSize 15 //size of the board
+//keys
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <conio.h>
 #include <string.h>
+#include "consts.h"
 #include "graphic/graphic.h"
 #include "user/user.h"
 #include "table/table.h"
