9769b9ee08ab9312258ec6ed3a917bae8fd3d460
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2020-12-19 13:42:17 +0330

    Fixed a little bug (now if you move out of board borders , you will come back in board , like , if you are on top and move to top , you will go to the bottom of board ) Thanks To ErenJeager80 for idea :D

--- move/move.c
+++ move/move.c
@@ -13,15 +13,19 @@ int move(){
         switch(ch){
             case KEY_UP:
             if(i>1) i--;
+            else i= boardSize;
             break;
             case KEY_DOWN:
             if(i<boardSize) i++;
+            else i= 1;
             break;
             case KEY_LEFT:
             if(j>1) j--;
+            else j= boardSize;
             break;
             case KEY_RIGHT:
             if(j<boardSize) j++;
+            else j= 1;
             break;
         }
         switch(temp){
