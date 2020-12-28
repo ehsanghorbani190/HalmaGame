ed1bb14a1d41a4210db77b733d182b4cb68e5238
shafiei1380 <M.shafiei.1380@gmail.com>
2020-12-28 07:24:38 -0800

    StartSocketProgramming

---
+++ socket/socket.c
@@ -0,0 +1,46 @@
+#include <winsock2.h>
+#include <windows.h>
+
+#pragma comment (lib, "Ws2_32.lib")
+
+SOCKET InitServer(){
+    WSADATA wsa;
+	SOCKET s;
+	struct sockaddr_in server;
+
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+	{
+		printf("Failed. Error Code : %d",WSAGetLastError());
+		return 1;
+	}
+	
+	printf("Initialised.\n");
+	
+	//Create a socket
+	if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+	{
+		printf("Could not create socket : %d" , WSAGetLastError());
+	}
+
+	printf("Socket created.\n");
+	
+	//Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons( 8888 );
+	
+	//Bind
+	if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
+	{
+		printf("Bind failed with error code : %d" , WSAGetLastError());
+	}
+	
+	puts("Bind done");
+
+	listen(s , 3);
+
+    return s;
+}
+
+
