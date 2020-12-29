00c3cd8814890161063f7efc508d0467af6c2345
shafiei1380 <M.shafiei.1380@gmail.com>
2020-12-29 13:43:51 +0330

    FixBugOnWatForPlayer

--- main.c
+++ main.c
@@ -11,17 +11,16 @@
 int main()
 {
   SOCKET socket;
-  char host[50]="127.0.0.1";
+  char host[50] = "127.0.0.1";
   struct Client clients[4];
   printf("%s\n", LOGO);
   getch();
   struct User user;
-  user=ShowLogin();
+  user = ShowLogin();
   char menus[10][100] = {"New Game", "Continue Game", "Log", "Exit"}, menu, connectionType;
 
   menu = DrawMenu(menus, 4);
 
- 
   if (menu == 1)
   {
     printf("%d", 1);
@@ -39,10 +38,10 @@ int main()
       strcpy(menus[0], "Play On Localhost");
       strcpy(menus[1], "Play Over The Internet");
       connectionType = DrawMenu(menus, 2);
-     
+
       if (connectionType == 1)
       {
-        
+
         strcpy(menus[0], "I Want To Be Server");
         strcpy(menus[1], "I Want To Be Client");
         connectionType = DrawMenu(menus, 2);
@@ -53,24 +52,25 @@ int main()
           menu = DrawMenu(menus, 2);
           socket = InitServer(8888);
 
-          for (int i = 0; i < 2 * menu; i++)
+          for (int i = 0; i < 2 * menu - 1; i++)
           {
             clients[i] = WaitForClient(socket);
           }
-          for (int i = 0; i <2 * menu; i++)
+          for (int i = 0; i < 2 * menu - 1; i++)
           {
-            printf("Player %d is: %s \n",i+1,clients[i].name);
+            printf("Player %d is: %s \n", i + 1, clients[i].name);
           }
-        }else
+        }
+        else
         {
-          
+
           printf("plz input your host ip [do you want use default setting?(y/n)]:");
-          scanf("%s",host);
-           if(!strcmp(host,"y") ||!strcmp(host,"Y"))
-            strcpy(host,"127.0.0.1");
-         
-            socket=InititClient(host,8888);
-            Send(socket,user.userName);
+          scanf("%s", host);
+          if (!strcmp(host, "y") || !strcmp(host, "Y"))
+            strcpy(host, "127.0.0.1");
+
+          socket = InititClient(host, 8888);
+          Send(socket, user.userName);
         }
       }
     }
