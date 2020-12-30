d086e642b550828b4ca30abec868a6b4e041aabc
Soroush Fathi <soroushfathi6666@gmail.com>
2020-12-30 09:53:32 +0330

    add the new type of bead position

--- table/table.c
+++ table/table.c
@@ -1,47 +1,41 @@
 #include <stdio.h>
 
-#define gotoxy(x,y) printf("\033[%d;%dH", (y), (x))
-
 int addBead(int PlayerCount)
 {
     // 0 >> empty house
     for (int i = 0; i < 10; i++)
-    {
         for (int j = 0; j < 10; j++)
           boardBead[i][j] = 0;
-    }
     // 1 >> player 1
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 3 - i; j >= 0; j--)
+    for (int i = 0; i < beadType; i++)
+        for (int j = beadType - 1 - i; j >= 0; j--)
             boardBead[i][j] = 1;
-    }
-    for (int i = 1, j = 3; i < 4, j > 0; i++, j--)
+    for (int i = 1, j = beadType - 1; i < beadType, j > 0; i++, j--)
         boardBead[i][j] = 1;
     // 2 >> player 2
-    for (int i = boardSize - 4 ; i < boardSize ; i++)
-        for (int j = boardSize - 1 ; j > boardSize - 5; j--)
-            if(i + j  > 2 * boardSize - 6)
+    for (int i = boardSize - beadType ; i < boardSize ; i++)
+        for (int j = boardSize - 1 ; j > boardSize - beadType - 1; j--)
+            if(i + j  > 2 * boardSize - beadType - 2)
                boardBead[i][j] = 2;
-    for (int i = boardSize -2, j = boardSize - 4; i >= boardSize - 4, j <= boardSize -2; i--, j++)
+    for (int i = boardSize -2, j = boardSize-beadType; i >= boardSize-beadType, j <= boardSize-2; i--, j++)
         boardBead[i][j] = 2;
     if(PlayerCount==4){
     // 3 >> player 3
         int k = 0;
-    for (int i = boardSize - 4; i < boardSize ; i++)
+    for (int i = boardSize - beadType; i < boardSize ; i++)
     {
         for (int j = 0; j <= k; j++)
             boardBead[i][j] = 3;
         k++;    
     }
-    for (int i = boardSize - 2, j = 3; i >= boardSize - 4, j > 0; i--, j--)
+    for (int i = boardSize - 2, j = beadType-1; i >= boardSize - beadType, j > 0; i--, j--)
         boardBead[i][j] = 3;     
     // 4 >> player 4
-    for (int i = 3; i >= 0; i--)
-        for (int j = boardSize - 1 ; j > boardSize - 5; j--)
-            if(j - i > boardSize - 5)
+    for (int i = beadType - 1; i >= 0; i--)
+        for (int j = boardSize-1 ; j > boardSize-beadType-1; j--)
+            if(j - i > boardSize-beadType-1)
                boardBead[i][j] = 4;
-    for (int i = 1, j = boardSize - 4; i < 3, j < boardSize -1; i++, j++)
+    for (int i = 1, j = boardSize - beadType; i < beadType-1, j < boardSize -1; i++, j++)
         boardBead[i][j] = 4;     
     }
 }
@@ -84,7 +78,7 @@ void board()
             char ch;
             if(boardBead[i][j] > 0) ch = 254;
             else if(boardBead[i][j] == 0) ch = 32;
-            else if(boardBead[i][j] == -1) ch = 240; 
+            //else if(boardBead[i][j] == -1) ch = 240; 
             printf("  |  ");
             switch(boardBead[i][j]){
                 case 1: 
