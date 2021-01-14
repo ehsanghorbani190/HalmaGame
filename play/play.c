8099620160abc8d159157be125621f88818b0448
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2021-01-14 13:02:44 +0330

    Implmented the repeat draw mode , fixed some tiny bugs

--- play/play.c
+++ play/play.c
@@ -199,10 +199,46 @@ void play()
     int i = 1, j = 1, temp = boardBead[i - 1][j - 1], x, y, fx, fy, tx, ty, BeadReplace = 0 , continues = 0;
     char ch, tch;
     int moveCount[playerCount] = {0}, swTurn = 1;
+    int lm[playerCount][2]={{0}};
+    int lmt[2];
+    int RepeatCount = 0 , RepeatSwitch = 0 , drawOffer = 0;
     //we want to stop the game if we press esc button
     while (ch != 27)
     {
         //getting the bead the player wants to move
+        if(RepeatSwitch) {
+            RepeatSwitch = 0;
+            gotoxy(0,50);
+            printf("Dear Player %d The board is in a repeating sequence ! Do you want to offer Draw to your opponent ?(Y:yes / Anything else : No)", Role);
+            char ans =getch();
+            if(ans== 'y' || ans =='Y') {
+                drawOffer= 1;
+                Role = (Role < playerCount) ? Role + 1 : 1;
+                continue;
+            }
+            else{
+                drawOffer =0 ;
+            }
+        }
+        if(drawOffer){
+            drawOffer = 0;
+            gotoxy(0,50);
+            printf("Dear Player %d Your opponent is offering draw to you , due to board state is repeating. Do You Accept ?(Y:yes / Anything else : No)" , Role);
+            char ans =getch();
+            if(ans == 'y' || ans =='Y'){
+                if(playerCount == 2){
+                    Log("The Game is A DRAW :D , Thanks for playing , press any button to exit :D");
+                    getch();
+                    exit(0);
+                }
+            }
+            else {
+                Role = (Role < playerCount) ? Role + 1 : 1;
+                for(int k = 0 ; k <100; k++) printf("\b");
+                printf("Dear Player %d , Your opponent declined your offer :D" , Role);
+                continue;
+                }
+        }
         while (ch != 13 && ch != 27 && !continues)
         {
             x = j - 1, y = i - 1;
@@ -233,6 +269,7 @@ void play()
         gotoxy(7 + (j - 1) * 4, 3 + (i - 1) * 2);
         printf("\b%c", ' ');
         fx = j, fy = i;
+        lmt[0] = (fx*1000) + fy;
         int movingBead = boardBead[i - 1][j - 1];
         boardBead[i - 1][j - 1] = 0;
         ch = (ch == 27) ? ch : '\0';
@@ -307,17 +344,18 @@ void play()
         gotoxy(7 + (tx - 1) * 4, 3 + (ty - 1) * 2);
         printf("\b%c", 254);
         gotoxy(0, 50);
-        for (int w = 1; w <= 50; w++)
-            printf("\b");
         SetColor(15);
-        printf("Player %d Moved From %d %d to %d %d\n", Role, fy, fx, ty, tx);
         moveCount[swTurn - 1]++;
-        printf("!Player %d Moved %d Times Up To Now!", swTurn, moveCount[swTurn - 1]);
+        printf("Player %d Moved %d Times Up To Now!\n", swTurn, moveCount[swTurn - 1]);
+        for (int w = 1; w <= 50; w++)
+            printf(" ");
         if (swTurn < playerCount)
             swTurn++;
         else
             swTurn = 1;
-        if(moveCount[0]+moveCount[1]+moveCount[2]+moveCount[3]>=leaveCampValue){
+        int sumMove = 0;
+        for (int w = 0; w < playerCount;w++) sumMove += moveCount[w]; 
+        if(sumMove>=leaveCampValue){
             if (winnerChecker_type1() == 1)
                 printf("Player 1 Won :]");
             else if (winnerChecker_type1() == 2)
@@ -328,7 +366,7 @@ void play()
                 printf("Player 4 Won :]");
             else if (winnerChecker_type1() == 0);
         }
-        if(moveCount[0]+moveCount[1]+moveCount[2]+moveCount[3] >= leaveCampValue){
+        if(sumMove >= leaveCampValue){
             if (winnerChecker_type2() == 1)
                 printf("Player 1 Won :]");
             else if (winnerChecker_type2() == 2)
@@ -344,7 +382,18 @@ void play()
                 Log("You may move again , For finishing your turn please press F button");
                 continue;
             }
-        Role = (Role < 4) ? Role + 1 : 1;
+        lmt[1] = (tx*1000) + ty;
+        if(lm[Role-1][0] == 0) RepeatSwitch = 0;
+        else if(lm[Role-1][0] == lmt[1] && lm[Role-1][1] == lmt[0]) {
+            RepeatCount++;
+            RepeatSwitch = 0;
+            }
+        else {RepeatCount = 0; RepeatSwitch = 0;}
+            lm[Role-1][0] = lmt[0];
+            lm[Role-1][1] = lmt[1];
+        if(RepeatCount == playerCount) RepeatSwitch = 1;
+        else RepeatSwitch=0;
+        Role = (Role < playerCount) ? Role + 1 : 1;
         ch = (ch == 27) ? ch : '\0';
     }
 }
